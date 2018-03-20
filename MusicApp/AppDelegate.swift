09f958a9ed4bd7c2d1ba3b85028cc158add568e6
Prasad G <PrasadG@client.openvpn.net>
2018-03-20 17:05:19 +0530

    First commit

--- MusicApp/AppDelegate.swift
+++ MusicApp/AppDelegate.swift
@@ -13,9 +13,7 @@ public let STORYBOARD = UIStoryboard(name: "Main", bundle: nil)
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
